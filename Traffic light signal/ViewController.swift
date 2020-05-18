434683ece2b3a1222259ca23689631d9c7351734
Vitalii Sosin <375693@mail.ru>
2020-05-19 02:59:20 +0300

    I did traffic light signal for all device

--- Traffic light signal/ViewController.swift
+++ Traffic light signal/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redColorLabel: UIView!
+    @IBOutlet weak var yellowColorLabel: UIView!
+    @IBOutlet weak var greenColorLabel: UIView!
+    @IBOutlet weak var startButtonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redColorLabel.alpha = 0.3
+        redColorLabel.layer.cornerRadius = 50
+        
+        yellowColorLabel.alpha = 0.3
+        yellowColorLabel.layer.cornerRadius = 50
+        
+        greenColorLabel.alpha = 0.3
+        greenColorLabel.layer.cornerRadius = 50
     }
 
-
+    @IBAction func startButtonAction() {
+        
+        if redColorLabel.alpha < 1 {
+            redColorLabel.alpha = 1
+            startButtonLabel.setTitle("NEXT", for: .normal)
+            
+        } else if yellowColorLabel.alpha < 1{
+            yellowColorLabel.alpha = 1
+            
+        } else if greenColorLabel.alpha < 1 {
+            greenColorLabel.alpha = 1
+            
+        } else if greenColorLabel.alpha == 1 {
+            redColorLabel.alpha = 0.3
+            yellowColorLabel.alpha = 0.3
+            greenColorLabel.alpha = 0.3
+            startButtonLabel.setTitle("Start", for: .normal)
+        }
+    }
 }
 
