f1c40433a6dbb1e0a0306c89bcd2c2c086fb9af3
Vitalii Sosin <375693@mail.ru>
2020-05-19 01:48:07 +0300

    Initial Commit

---
+++ Traffic light signal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Traffic light signal
+//
+//  Created by Виталий Сосин on 19.05.2020.
+//  Copyright © 2020 Vitalii Sosin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
